d12158f6f7cc9ad2eadeb3c5154dabdccd0e787f
Hyungseock-Yun <bagzein@gmail.com>
2023-03-22 02:48:31 +0900

    - 프록시 기술과 한계 > 의존관계 주입

--- src/test/java/hello/aop/proxyvs/code/ProxyDIAspectTest.java
+++ src/test/java/hello/aop/proxyvs/code/ProxyDIAspectTest.java
@@ -13,7 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 @Import(ProxyDIAspect.class)
 //@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"})   // JDK 동적 프록시
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"})   // CGLIB 동적 프록시
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"})   // CGLIB 동적 프록시
+  @SpringBootTest   // 기본 CGLIB
 class ProxyDIAspectTest {
 
   @Autowired
